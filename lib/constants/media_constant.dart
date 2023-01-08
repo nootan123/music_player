b386f38ea417e47cf010beb3ad20859f0941089b
Nootan Bhattarai <mee.nootan@gmail.com>
2023-01-08 16:07:53 +0545

    updated image path

--- lib/constants/media_constant.dart
+++ lib/constants/media_constant.dart
@@ -3,4 +3,5 @@ import 'dart:typed_data';
 List<int> intList = [3, 122, 23, 23, 123];
 Uint8List placeholderCover = Uint8List.fromList(intList);
 String errorReplaceCover = 'lib/assets/music_cover.jpg';
+String albumCover = 'lib/assets/album.jfif';
 String unknownAlbum = 'unknown';

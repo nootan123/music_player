bbd6e2fac34fb9a00a22482744d20ee07ad8636e
Nootan Bhattarai <mee.nootan@gmail.com>
2023-01-08 16:09:06 +0545

    added box shadow

--- lib/screens/album/albums.dart
+++ lib/screens/album/albums.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:player/constants/media_constant.dart';
 import 'package:player/provider/media_controller_provider.dart';
 import 'package:player/screens/album/album_list.dart';
 import 'package:provider/provider.dart';
@@ -11,8 +12,6 @@ class AlbumTab extends StatefulWidget {
 }
 
 class _AlbumTabState extends State<AlbumTab> {
- 
-
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
@@ -24,12 +23,49 @@ class _AlbumTabState extends State<AlbumTab> {
           return InkWell(
             child: Card(
               color: Colors.amber,
-              child: Column(
+              child: Stack(
                 children: [
-                  Text(
-                      '${Provider.of<MediaControllerProvider>(context).albums[index].albumName}'),
-                  Text(
-                      '${Provider.of<MediaControllerProvider>(context).albums[index].songList?.length} songs'),
+                  Image.asset(
+                    albumCover,
+                    fit: BoxFit.cover,
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.black26,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey,
+                            blurRadius:
+                                10.0,
+                            spreadRadius:
+                                5.0,
+                            offset: Offset(
+                              0.0,
+                              0.0,
+                            ),
+                          )
+                        ],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                                '${Provider.of<MediaControllerProvider>(context).albums[index].albumName}',
+                                style: const TextStyle(color: Colors.white)),
+                            Text(
+                                '${Provider.of<MediaControllerProvider>(context).albums[index].songList?.length} songs',
+                                style: const TextStyle(color: Colors.white)),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
                 ],
               ),
             ),
